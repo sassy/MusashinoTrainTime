c7210745d5a8e5c26e42338c36094301c96e26b6
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-09-03 07:07:44 +0900

    add base program.

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+const (
+	DefaultNumOfResultToShow = 3
+)
+
+type Time struct {
+	hour, minute int
+}
+
+func (time *Time) toString() string {
+	if time.minute < 10 {
+		return fmt.Sprintf("%d:0%d ", time.hour, time.minute)
+	} else {
+		return fmt.Sprintf("%d:%d ", time.hour, time.minute)
+	}
+}
+
+func departureTime(departure string) (int, int) {
+	ret, _ := regexp.MatchString("^[0-9]{1,2}:[0-9]{1,2}$", departure)
+	var hour int
+	var minute int
+	if ret {
+		re := regexp.MustCompile("^([0-9]{1,2}):([0-9]{1,2})$")
+		bs := []byte(departure)
+		group := re.FindSubmatch(bs)
+		h, _ := strconv.Atoi(string(group[1]))
+		m, _ := strconv.Atoi(string(group[2]))
+		if h >= 0 && h < 24 && m >= 0 && m < 60 {
+			hour = h
+			minute = m
+		} else {
+			now := time.Now()
+			hour = now.Hour()
+			minute = now.Minute()
+		}
+	} else {
+		now := time.Now()
+		hour = now.Hour()
+		minute = now.Minute()
+	}
+	return hour, minute
+}
+
+func createTimetable() []Time {
+	var timetable = make([]Time, 0)
+	doc, _ := goquery.NewDocument("http://www.jreast-timetable.jp/1509/timetable/tt0413/0413020.html")
+	doc.Find(".timetable .result_03").Each(func(_ int, s *goquery.Selection) {
+		s.Find("tbody tr").Each(func(_ int, s *goquery.Selection) {
+			key, err := strconv.Atoi(s.Find("td:nth-child(1)").Text())
+			if err == nil {
+				s.Find("td:nth-child(2) div.timetable_time[data-dest='府']").Each(func(_ int, s *goquery.Selection) {
+					value, _ := strconv.Atoi(s.Find(".minute").Text())
+					timetable = append(timetable, Time{key, value})
+				})
+			}
+		})
+	})
+	return timetable
+}
+
+func printTimes(times []string) {
+	for _, v := range times {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	var (
+		departure   string
+		numOfResult int
+		isLast      bool
+	)
+	flag.StringVar(&departure, "t", "", "specify departure time.")
+	flag.BoolVar(&isLast, "l", false, "show last bus of the day.")
+	flag.IntVar(&numOfResult, "n", DefaultNumOfResultToShow, "specify amount of result.")
+	flag.Parse()
+
+	if numOfResult < 0 {
+		fmt.Fprintf(os.Stderr, "parameter for -n must be greater than 0.\n")
+		os.Exit(2)
+	}
+
+	hour, minute := departureTime(departure)
+	timetable := createTimetable()
+
+	if isLast {
+		result := []string{timetable[len(timetable)-1].toString()}
+		printTimes(result)
+		return
+	}
+
+	result := make([]string, 0, numOfResult)
+	for i := 0; i < len(timetable); i++ {
+		v := timetable[i]
+		if v.minute > minute && v.hour >= hour {
+			timeStr := v.toString()
+			result = append(result, timeStr)
+			if len(result) >= numOfResult {
+				break
+			}
+		}
+	}
+	printTimes(result)
+}
